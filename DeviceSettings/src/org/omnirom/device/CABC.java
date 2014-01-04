8483bfe6c1b8232da93db8fc4fd4e8da2012ce9f
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2014-01-05 01:29:19 +0700

    Allow override of cabc file via property
    Credits: sbrissen <sbrissen@hotmail.com>

--- DeviceSettings/src/org/omnirom/device/CABC.java
+++ DeviceSettings/src/org/omnirom/device/CABC.java
@@ -23,6 +23,7 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceManager;
 import android.util.AttributeSet;
+import android.os.SystemProperties;
 
 public class CABC extends CheckBoxPreference implements OnPreferenceChangeListener {
 
@@ -31,7 +32,7 @@ public class CABC extends CheckBoxPreference implements OnPreferenceChangeListen
     public CABC(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.setOnPreferenceChangeListener(this);
-        FILE = context.getResources().getString(R.string.mdnie_cabc_sysfs_file);
+        FILE = SystemProperties.get("ro.omni.hardware.cabc", "/sys/class/lcd/panel/power_reduce");
     }
 
     public static boolean isSupported(String filePath) {
@@ -43,7 +44,7 @@ public class CABC extends CheckBoxPreference implements OnPreferenceChangeListen
      * @param context       The context to read the SharedPreferences from
      */
     public static void restore(Context context) {
-        FILE = context.getResources().getString(R.string.mdnie_cabc_sysfs_file);
+        FILE = SystemProperties.get("ro.omni.hardware.cabc", "/sys/class/lcd/panel/power_reduce");
         if (!isSupported(FILE)) {
             return;
         }
