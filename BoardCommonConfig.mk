7f9c4d4e7d65e4d341405c7474f2244629f5198e
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2014-01-08 16:39:13 +0700

    lt01-common: Common source codes reworked

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -16,144 +16,49 @@
 
 # This variable is set first, so it can be overridden
 # by BoardConfigVendor.mk
-USE_CAMERA_STUB := true
-BOARD_USES_GENERIC_AUDIO := false
-
-TARGET_BOOTANIMATION_PRELOAD := true
-
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a9
-TARGET_CPU_VARIANT := cortex-a9
-ARCH_ARM_HAVE_NEON := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
-
-EXYNOS4X12_ENHANCEMENTS := true
-EXYNOS4_ENHANCEMENTS := true
-
-ifdef EXYNOS4X12_ENHANCEMENTS
-COMMON_GLOBAL_CFLAGS += -DEXYNOS4_ENHANCEMENTS
-COMMON_GLOBAL_CFLAGS += -DEXYNOS4X12_ENHANCEMENTS
-COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
-endif
-
-BOARD_VENDOR := samsung
-TARGET_BOARD_PLATFORM := exynos4
-TARGET_SOC := exynos4x12
-TARGET_BOOTLOADER_BOARD_NAME := smdk4x12
-
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
-TARGET_PROVIDES_INIT := true
-TARGET_PROVIDES_INIT_TARGET_RC := true
+-include device/samsung/smdk4412-common/BoardCommonConfig.mk
 
 # Kernel
-BOARD_KERNEL_CMDLINE := "console=ttySAC2,115200"
 BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_PAGESIZE := 2048
 
 # Filesystem
-BOARD_NAND_PAGE_SIZE := 4096
-BOARD_NAND_SPARE_SIZE := 128
-BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2235564032
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12813598720
-BOARD_FLASH_BLOCK_SIZE := 4096
-TARGET_USERIMAGES_USE_EXT4 := true
-
-# Hardware tunables
-BOARD_HARDWARE_CLASS := hardware/samsung/cmhw
-
-# Graphics
-BOARD_EGL_CFG := device/samsung/lt01-common/configs/egl.cfg
-USE_OPENGL_RENDERER := true
-BOARD_USES_SKIAHWJPEG := true
-COMMON_GLOBAL_CFLAGS += -DSEC_HWJPEG_G2D -DFORCE_SCREENSHOT_CPU_PATH -DWORKAROUND_BUG_10194508
-TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
-
-# FIMG Acceleration
-BOARD_USES_FIMGAPI := true
-BOARD_USES_SKIA_FIMGAPI := true
-
-# Enable WEBGL in WebKit
-ENABLE_WEBGL := true
-
-# HWComposer
-BOARD_USES_HWCOMPOSER := true
-BOARD_USE_SYSFS_VSYNC_NOTIFICATION := true
-
-# TVOut & HDMI
-BOARD_USE_SECTVOUT := true
-BOARD_USES_SKTEXTBOX := true
-
-# Camera
-BOARD_CAMERA_HAVE_ISO := true
-COMMON_GLOBAL_CFLAGS += -DHAVE_ISO
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_DVFS
-BOARD_USES_PROPRIETARY_LIBFIMC := true
-
-# OMX
-BOARD_USE_SAMSUNG_COLORFORMAT := true
-BOARD_NONBLOCK_MODE_PROCESS := true
-BOARD_USE_STOREMETADATA := true
-BOARD_USE_METADATABUFFERTYPE := true
-BOARD_USES_MFC_FPS := true
-BOARD_USE_S3D_SUPPORT := true
-BOARD_USE_CSC_FIMC := false
-
-# RIL
-BOARD_MOBILEDATA_INTERFACE_NAME := "pdp0"
-
-# Wifi
-BOARD_WLAN_DEVICE                := bcmdhd
-BOARD_WLAN_DEVICE_REV            := bcm4334
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/dhd.ko"
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/wifi/bcmdhd_p2p.bin"
-WIFI_DRIVER_MODULE_NAME          := "dhd"
-WIFI_DRIVER_MODULE_ARG           := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
-WIFI_DRIVER_MODULE_AP_ARG        := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
-WIFI_BAND                        := 802_11_ABG
-BOARD_HAVE_SAMSUNG_WIFI          := true
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_HAVE_SAMSUNG_BLUETOOTH := true
-BOARD_BLUEDROID_VENDOR_CONF := device/samsung/lt01-common/bluetooth/vnd_smdk4x12.txt
-
-# Vold
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
 
 # Recovery
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/lt01-common/recovery/recovery_keys.c
-BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/lt01-common/recovery/graphics.c
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
-BOARD_USES_MMCUTILS := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_RECOVERY_SWIPE := true
 
-# Charging mode
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-BOARD_BATTERY_DEVICE_NAME := "battery"
+# Sensors
+BOARD_USE_LEGACY_SENSORS_FUSION := false
+
+# Legacy driver compatibility
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+
+# Button backlight
+BOARD_EXYNOS4X12_TABLET_HAS_LED_BUTTONS := true
 
 # inherit from the proprietary version
 -include vendor/samsung/lt01-common/BoardConfigVendor.mk
+
+# Selinux
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/lt01-common/selinux
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    te_macros \
+    device.te \
+    dhcp.te \
+    domain.te \
+    file.te \
+    init.te \
+    kickstart.te \
+    mediaserver.te \
+    netmgrd.te \
+    qmux.te \
+    rild.te \
+    secril.te \
+    system.te \
+    ueventd.te \
+    wpa_supplicant.te
