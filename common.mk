68123ed5ddfb3157f0fbc7e14bc2254d7425d79e
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2014-03-06 18:52:17 +0700

    lt01-common: Add f2fs filesystem support
    Change-Id: I1aa66eae0c1624d0111c5bf027cc506e61355259

--- common.mk
+++ common.mk
@@ -61,6 +61,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     tinyplay
 
+# F2FS filesystem
+PRODUCT_PACKAGES += \
+    mkfs.f2fs \
+    fsck.f2fs \
+    fibmap.f2fs \
+    f2fstat
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.cm.hardware.cabc=/sys/class/mdnie/mdnie/cabc
 
